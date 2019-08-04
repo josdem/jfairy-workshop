29f7d46c132d6b02d29005278fa268e00a69eee8
josdem <joseluis.delacruz@gmail.com>
2019-08-04 12:23:17 -0400

    Adding person test

---
+++ src/test/java/com/jos/dem/jfairy/workshop/PersonTest.java
@@ -0,0 +1,20 @@
+package com.jos.dem.jfairy.workshop;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import java.util.logging.Logger;
+
+class PersonTest {
+
+  private static final Logger log = Logger.getLogger(PersonTest.class.getName());
+
+  @Test
+  @DisplayName("Should validate person")
+  void shouldValidatePerson() {
+    assertTrue(false);
+  }
+
+}
