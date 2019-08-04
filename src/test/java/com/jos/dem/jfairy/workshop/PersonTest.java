8f499e6dcedf3749f713958197c4c0d6eef7d7a6
josdem <joseluis.delacruz@gmail.com>
2019-08-04 12:47:24 -0400

    Validating person information

--- src/test/java/com/jos/dem/jfairy/workshop/PersonTest.java
+++ src/test/java/com/jos/dem/jfairy/workshop/PersonTest.java
@@ -1,6 +1,10 @@
 package com.jos.dem.jfairy.workshop;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertAll;;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.devskiller.jfairy.Fairy;
+import com.devskiller.jfairy.producer.person.Person;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
@@ -9,12 +13,20 @@ import java.util.logging.Logger;
 
 class PersonTest {
 
+  private Person person = Fairy.create().person();
+
   private static final Logger log = Logger.getLogger(PersonTest.class.getName());
 
   @Test
   @DisplayName("Should validate person")
   void shouldValidatePerson() {
-    assertTrue(false);
+    log.info("Running: Should validate person information");
+
+    assertAll("person",
+        () -> assertNotNull(person.getFirstName(), "Should get person's name"),
+        () -> assertNotNull(person.getEmail(), "Should get person's email"),
+        () -> assertNotNull(person.getTelephoneNumber(), "Should get person's telephone number")
+        );
   }
 
 }
